0e4166b507949cceb7fa843a8b1ffde4091a37ea
Ronak Hirpara <ronakh130@gmail.com>
2024-04-17 14:17:56 -0400

    Fix entry file for expo

--- src/client/App.js
+++ src/client/App.js
@@ -6,7 +6,7 @@ import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import { MoodReportModal } from './components/MoodReport/MoodReportModal';
 
-export function App() {
+export default function App() {
   return (
     <StrictMode>
       <Provider store={store}>
