f8bf87e99adf7f5d50a184fd9ff0eaa79f73e23c
Ronak Hirpara <ronakh130@gmail.com>
2024-05-16 15:20:40 -0400

    Add supabase

--- src/client/App.js
+++ src/client/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, View } from 'react-native';
+import { AppState, SafeAreaView, StyleSheet, View } from 'react-native';
 import { Calendar } from './components/Calendar/Calendar';
 import { StrictMode } from 'react';
 import { Provider } from 'react-redux';
@@ -9,20 +9,32 @@ import { CalendarPage } from './pages/CalendarPage';
 import { colors } from './utils/styles';
 import { LoginPage } from './pages/LoginPage';
 import { SignUpPage } from './pages/SignUpPage';
+import { supabase } from './lib/supabase';
+import { registerRootComponent } from 'expo';
 
-export default function App() {
+AppState.addEventListener('change', (state) => {
+  if (state === 'active') {
+    supabase.auth.startAutoRefresh();
+  } else {
+    supabase.auth.stopAutoRefresh();
+  }
+});
+
+function App() {
   return (
     <StrictMode>
       <Provider store={store}>
         <SafeAreaView style={styles.page}>
-          <CalendarPage />
-          {/* <LoginPage /> */}
+          {/* <CalendarPage /> */}
+          <LoginPage />
           {/* <SignUpPage /> */}
         </SafeAreaView>
       </Provider>
     </StrictMode>
   );
 }
+
+export default registerRootComponent(App);
 
 const styles = StyleSheet.create({
   page: {
