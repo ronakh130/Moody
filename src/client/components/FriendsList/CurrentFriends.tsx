23ff38caa7ec08e9f652a6b3e4c40044f022922b
Ronak Hirpara <ronakh130@gmail.com>
2024-06-17 18:37:04 -0400

    Show current friends

--- src/client/components/FriendsList/CurrentFriends.tsx
+++ src/client/components/FriendsList/CurrentFriends.tsx
@@ -1,13 +1,24 @@
 import { StyleSheet, View } from 'react-native';
 import { FriendRow } from './FriendRow';
 import { baseCenterFlexStyle } from '../../utils/styles';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../redux/store';
 
 export const CurrentFriends = () => {
+  const { friendships } = useSelector(
+    (state: RootState) => state.friendReducer
+  );
+
   return (
     <View style={styles.container}>
       <FriendRow name='Ronak' status='accepted' />
       <FriendRow name='Sam' status='accepted' />
       <FriendRow name='Sojo' status='accepted' />
+      {friendships
+        .filter((fs) => fs.status === 'accepted')
+        .map((fs) => {
+          return <FriendRow name={fs.name ?? fs.email} status={fs.status} />;
+        })}
     </View>
   );
 };
